1003aca994734e8054657712ba223829fc57970a
lianghua1987 <hua_liang@mckinsey.com>
2023-08-27 21:21:24 +0800

    Add testing

---
+++ basic-testing/index.mocha.test.js
@@ -0,0 +1,33 @@
+const assert = require('assert')
+
+const {forEach, map} = require('./index');
+const {color} = require("../util");
+
+
+const test = (desc, fn) => {
+  console.log(`Start testing '${desc}'`);
+  try {
+    fn();
+    console.log(color.BgGreen + color.END, 'Passed')
+  } catch (err) {
+    console.log(color.BgRed + color.END, 'Failed', err);
+  }
+};
+
+it('forEach', () => {
+  let sum = 0;
+  forEach([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], v => sum += v);
+  assert.strictEqual(sum, 55, "Expected summing array to equal 55");
+});
+
+it('map', () => {
+  const result = map([1, 2, 3], v => v * 2);
+  assert.strictEqual(2, result[0], `Expected result is 2, but found ${result[0]}`);
+  assert.strictEqual(4, result[1], `Expected result is 2, but found ${result[1]}`);
+  assert.strictEqual(6, result[2], `Expected result is 2, but found ${result[2]}`);
+  assert.deepStrictEqual(result, [2, 4, 6]);
+});
+
+
+
+
